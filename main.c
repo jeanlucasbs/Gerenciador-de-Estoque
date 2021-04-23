9c4e887470d2b77b188c092bf75c3e4e9f57814e
Jean <50235366+uaijean@users.noreply.github.com>
2021-04-23 11:19:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+/* Estrutura de Dados 1
+   Alunos: Jean Lucas Barbosa Silva
+           Pedro Richardt Kall
+*/
 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_PRODUTO 100
+#define MAX_CATEGORIA 50
+#define MAX_NOME 5
